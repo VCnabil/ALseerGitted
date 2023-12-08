d1f20c593ca40b9848e2703ccd92a89bc91d1507
Nabil <nlamriben@vcinc.com>
2023-12-08 13:58:51 -0500

    first

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlSeerGui
+{
+    public partial class Form1 : Form
+    {
+        CanManager canManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+            canManager = new CanManager();
+            canManager.ListChannels();
+            canManager.OpenChannel(0);
+            canManager.SetBusParams();
+            canManager.GoOnBus();
+
+            canManager.SendTestCan();
+        }
+    }
+}
